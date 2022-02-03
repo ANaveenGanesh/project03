888c0a539ea3973121f606e6ca575e2c0dc83aa5
ANaveenGanesh <97721098+ANaveenGanesh@users.noreply.github.com>
2022-02-03 09:44:17 +0530

    Add files via upload

---
+++ user.js
@@ -0,0 +1,23 @@
+angular.module("User",[])
+.filter("myfilter",function()
+{
+    return function(input,option)
+    {
+        if(isNaN(option)||(option==""))
+        {
+            return input;
+        }
+        else {
+        return input.substring(0,option).toUpperCase();
+    }}
+})
+.controller("userCtrl",function($scope)
+{
+    var users=[
+        {name:'William,,dob:new Date("November 5, 1932"),Gender:"Male",Salary:4000},
+        {name:'Jack',dob:new Date(""),Gender:"Male",Salary:2000},
+    ];
+    $scope.users=users;
+    $scope.rowlimit=20;
+
+});
