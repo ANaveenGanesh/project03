31cb9dfd57cf1d0a3f09b2189d2eb57bb31534b1
ANaveenGanesh <97721098+ANaveenGanesh@users.noreply.github.com>
2022-02-03 12:19:46 +0530

    Update user.js

--- user.js
+++ user.js
@@ -15,7 +15,7 @@ angular.module("User",[])
 {
     var users=[
         {name:'William,,dob:new Date("November 5, 1932"),Gender:"Male",Salary:4000},
-        {name:'Jack',dob:new Date(""),Gender:"Male",Salary:2000},
+        {name:'Jack',dob:new Date("December 6,1931"),Gender:"Male",Salary:2000},
     ];
     $scope.users=users;
     $scope.rowlimit=20;
